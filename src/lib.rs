c9fa42c47634c4eba68c789e431a68cff97e48bd
박근수 <rmstn8426@gmail.com>
2024-03-05 17:00:45 +0900

    Thread pool

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,55 @@
 use std::{io, thread};
+use std::sync::{Arc, mpsc, Mutex};
+use std::sync::mpsc::Receiver;
 
-// struct Worker {
-//     id : usize,
-//     thread : thread::JoinHandle<()>
-// }
-
-pub struct ThreadPool{
-    threads : Vec<thread::JoinHandle<()>>
+struct Worker {
+    id : usize,
+    thread : thread::JoinHandle<Receiver<Job>>
 }
 
-impl ThreadPool{
-    pub fn new(size:usize) -> ThreadPool{
-        assert!(size>0);
-        // 패닉을 일으키는게 좋은 걸까? 에러 처리를 하는게 더 낫지 않을까?
-        let mut threads = Vec::with_capacity(size);
+impl Worker {
+    fn new(id:usize,receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move|| loop {
 
-        for _ in 0..size{
-            threads.push(thread::spawn(||{}))
-            // 여기에는 thread spawn이 생기는데 이전에는 무슨 일을 할지 클로저로 념겨줬었다.
-            // 하지만 클로저에 뭘 넣어야할까? 이후에 넘길 수는 없을까?
-            // 왜나하면 request가 오면 thread가 그 request를 처리하게 만들고 싶기 때문이다.
+            while let Ok(job) = receiver.lock().unwrap().recv(){
+                println!("Worker {id} got a job; executing.");
+                job();
+            }
+        });
+
+        Worker{id,thread}
+    }
+}
+
+pub struct ThreadPool{
+    workers : Vec<Worker>,
+    sender : mpsc::Sender<Job>
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool{
+    pub fn new(size:usize) -> ThreadPool {
+        assert!(size>0);
+
+        let (sender,receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        // 패닉을 일으키는게 좋은 걸까? 에러 처리를 하는게 더 낫지 않을까?
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size{
+            workers.push(Worker::new(id,Arc::clone(&receiver)));
         }
 
-        ThreadPool {threads}
+        ThreadPool {workers,sender}
     }
 
     pub fn execute<F>(&self, f:F)
     where
         F:FnOnce() + Send + 'static
     {
+        let job = Box::new(f);
+        self.send(job).unwrap();
 
     }
 }
